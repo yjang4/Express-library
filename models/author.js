3d66f0b1f62b8eee7d64829c16445d9cf3f5f25f
Daniel <mikyjo2012@gmail.com>
2022-07-29 17:30:21 -0500

    date fixed

--- models/author.js
+++ models/author.js
@@ -1,3 +1,4 @@
+const { DateTime } = require("luxon");
 var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;
@@ -31,11 +32,11 @@ AuthorSchema
 AuthorSchema.virtual('lifespan').get(function() {
   var lifetime_string = '';
   if (this.date_of_birth) {
-    lifetime_string = this.date_of_birth.getYear().toString();
+    lifetime_string = DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED);
   }
   lifetime_string += ' - ';
   if (this.date_of_death) {
-    lifetime_string += this.date_of_death.getYear()
+    lifetime_string += DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED);
   }
   return lifetime_string;
 });
